446df9cf0af15040103b58e754db0ee58f676b06
Stéphane Travostino <sph@combo.cc>
2024-05-29 10:54:58 +0100

    Runtime.assertEquals -> Runtime.assert

--- bootstrap/builtins/runtime.c
+++ bootstrap/builtins/runtime.c
@@ -1,18 +1,18 @@
 #include "builtins.h"
 
-object *runtime_assert_equals(object *self, object *left, object *right) {
+object *runtime_assert(object *self, object *left, object *right) {
   (void)self;
 
   object *cmp = send(left, intern("=="), right);
   if (cmp != singleton_true) {
-    panic("Assertion failed: %s != %s", inspect(left), inspect(right));
+    panic("Assertion failed: %s == %s", inspect(left), inspect(right));
   }
   return NULL;
 }
 
 void runtime_bootstrap(object *scope) {
   object *the_Runtime = object_derive(the_Object, sizeof(object));
-  object_set_method(the_Runtime, intern("assertEquals"), 2, runtime_assert_equals);
+  object_set_method(the_Runtime, intern("assert"), 2, runtime_assert);
 
   object_set_variable(scope, intern("Runtime"), the_Runtime);
 }
